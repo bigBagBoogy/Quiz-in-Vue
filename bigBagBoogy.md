c4b0023443248a7b552fa3c8ba4d04bcb736e15e
maarten <van_den_bergmaarten@hotmail.com>
2023-11-20 12:47:25 +0100

    added card

--- bigBagBoogy.md
+++ bigBagBoogy.md
@@ -2,7 +2,7 @@
 
 git init
 git add .
-git commit -m "added search"
+git commit -m "added card"
 git push -u origin main
 
 # todo:⭐️
--- src/App.vue
+++ src/App.vue
@@ -17,15 +17,16 @@ watch(search, () => {
       <div class="h1">quiz</div>
        <input v-model.trim="search" type="text" placeholder="Search...">
     </header> 
-      <div class="options-container"></div>
-        <div v-for="quiz in quizes" :key="quiz.id" class="card">
+      <div class="options-container">
+        <!-- <div v-for="quiz in quizes" :key="quiz.id" class="card">
           <img :src="quiz.img">
             <div class="card-text">
               <h2>{{quiz.name}}</h2>
               <p>{{quiz.questions.length}} questions  </p>
             </div>
-        </div>
+        </div> -->
       </div>
+    </div>
 </template>
 
 <style scoped>
@@ -44,7 +45,6 @@ header input {
   height: 25px;
   margin-left: 10px;
   padding: 5px;
-background-color: aqua;
   outline: none;
 }
 .h1 {
@@ -54,30 +54,12 @@ background-color: aqua;
 
 .options-container {
   margin-top: 40px;
+  display: grid;
+  grid-template-columns: auto auto auto auto;
+  gap: 10px;
+  padding: 10px;
 }
 
-.card {
-    width: 310px;
-    overflow: hidden;
-    border-radius: 2%;
-    box-shadow: 1px 1px 10px rgba(0,0,0,0.1);
-    margin-bottom: 35px;
-    margin-right: 20px;
-    cursor: pointer;
-  }
 
-  .card img {
-    width: 100%;
-    height: 190px;
-    margin: 0
-  }
-
-  .card .card-text {
-    padding: 0 5px
-  }
-
-  .card .card-text h2 {
-    font-weight: bold;
-  }
 
 </style>
